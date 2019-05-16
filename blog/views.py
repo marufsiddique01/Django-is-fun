8e924cc570f2bfbf26eef855143d87ef1c491e4b
Maruf Siddique <maruf.siddique@northsouth.edu>
2019-05-16 09:59:06 +0600

    Add files via upload

---
+++ blog/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render
+
+#from django.http import HttpResponse
+
+#dummy posts >>
+posts = [
+	{
+	'author': 'Maruf',
+	'title': 'Blog post 1',
+	'content': 'First post content',
+	'date_posted': 'August 27, 2018',
+	},
+	{
+	'author': 'MarufS',
+	'title': 'Blog post 2',
+	'content': 'Second post content',
+	'date_posted': 'August 28, 2018',
+	}
+]
+
+
+#loading a template>>
+def home(request):
+
+	context = {
+		'posts' : posts
+		}
+
+	return render(request, 'blog/home.html', context)
+	#return HttpResponse('<h1>Blog Home</h1>')
+
+def about(request):
+	return render(request, 'blog/about.html', {'title' : 'About'})
+	#return HttpResponse('<h1>About Us</h1>')
+
+'''def post(request):
+	#creating context dictionary >
+	context = {
+		'posts' : posts
+		}
+		#posts is accessible from template>>
+	return render(request, 'blog/post.html', context) 
+	'''
+
+# blog -> templates -> blog -> template.html
+
+
+
+
